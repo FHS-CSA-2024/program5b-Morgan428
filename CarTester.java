5d4341ade5cde21fda2ff1e35b0fdbee75779fb4
Morgan428 <MorganWare428@gmail.com>
2024-09-10 17:31:40 -0700

    Update CarTester.java
    Errors near the end X3

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+public class CarTester{
+    Scanner s = new Scanner(System.in);
+    public Car addCar(){
+        String name = s.nextLine();
+        double miles = s.nextDouble();
+        double gallons = s.nextDouble();
+        new Car(name, miles, gallons);
+        car.toString();
+        return;
+    }
+    public static void main(String[] args){
+        addCar();
+    }
+}
